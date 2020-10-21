8f2cbb01e87b7d58c25ad949bc48b5632f3a9aa1
WhatsMyPurpose <44020749+WhatsMyPurpose@users.noreply.github.com>
2020-10-22 00:30:45 +0100

    Add files via upload

---
+++ Replay.py
@@ -0,0 +1,37 @@
+import numpy as np
+from collections import deque
+import random
+
+
+class Memory:
+    def __init__(self, state, action, reward, new_state, done):
+        self.cur_state = state
+        self.action = action
+        self.reward = reward
+        self.new_state = new_state
+        self.done = done
+
+
+class ReplayBuffer:
+    def __init__(self, maxlen):
+        self.maxlen = maxlen
+        self.memory = deque(maxlen=maxlen)
+
+    def store_transition(self, state, action, reward, new_state, done):
+        self.memory.append((state, action, reward, new_state, done,))
+
+    def sample_buffer(self, batch_size, state_shape):
+        states, actions, rewards, new_states, dones = zip(*random.choices(self.memory,
+                                                                          k=batch_size))
+        states = np.array(states).reshape((batch_size,) + state_shape)
+        actions = np.array(actions, dtype='int').reshape(batch_size)
+        rewards = np.array(rewards).reshape(batch_size)
+        new_states = np.array(new_states).reshape((batch_size,) + state_shape)
+        dones = ~np.array(dones, dtype=np.bool).reshape(batch_size)
+        return states, actions, rewards, new_states, dones
+
+    def clear(self):
+        self.memory = deque(maxlen=self.maxlen)
+
+    def __len__(self):
+        return len(self.memory)
